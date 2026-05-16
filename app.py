7411387ccd64a70aefb7ba7393064a941e124120
RicardoCP18 <rmcp_18@hotmail.com>
2026-05-16 00:22:40 -0500

    Update Colab link to include sharing parameters

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ st.write("Nombre: RICARDO MARTIN CESPEDES PINO")
 st.write("Código ISIL: 71240691")
 
 # Link de Colab
-st.markdown("https://colab.research.google.com/drive/11oBppWOzXTqCWIlwP8E6jjoedXLwbvdF#scrollTo=029Io0Dn6mJj")
+st.markdown("https://colab.research.google.com/drive/11oBppWOzXTqCWIlwP8E6jjoedXLwbvdF?usp=sharing")
 
